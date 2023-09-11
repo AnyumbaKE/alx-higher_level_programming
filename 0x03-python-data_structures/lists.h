ae6d631ec6cfe6ea57417fdf35d24c4729ce3f88
AnyumbaKE <esanyumba@gmail.com>
2023-09-11 10:59:46 -0700

    palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
