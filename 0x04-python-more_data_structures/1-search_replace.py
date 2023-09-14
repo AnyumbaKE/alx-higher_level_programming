a0ef8d4f642f015fdcb152c182a6dfcb45e84ff1
AnyumbaKE <esanyumba@gmail.com>
2023-09-14 08:40:38 -0700

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+
+    if len(my_list) == 0:
+        return my_list
+
+    new_lst = [elem if elem != search else replace for elem in my_list]
+    return new_lst
