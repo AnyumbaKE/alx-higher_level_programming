fac0a4e68caa476069f11c2750604672fc718aef
Stan Anyumba <124333266+AnyumbaKE@users.noreply.github.com>
2023-09-25 22:26:02 +0300

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Write a function that prints x elements of a list.
 
+# Write a function that prints x elements of a list.
 
 def safe_print_list(my_list=[], x=0):
     total = 0
