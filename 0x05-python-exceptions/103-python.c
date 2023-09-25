031dc96656e29f165c6789b5d2425f5e79f78d49
AnyumbaKE <esanyumba@gmail.com>
2023-09-25 15:26:42 -0700

    p

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
 /**
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
