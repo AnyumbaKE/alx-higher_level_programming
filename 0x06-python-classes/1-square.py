80c50e014c3514fab302b0a031f28061c76c160d
AnyumbaKE <esanyumba@gmail.com>
2023-09-26 08:49:52 -0700

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square by:(based on 0-square.py"""
+
+
+class Square:
+    """This is an empty class"""
+    def __init__(self, size=None):
+        self.__size = size
