236ea958ab9e1c2e95d94dc4827ed81ac7578c90
AnyumbaKE <esanyumba@gmail.com>
2023-09-26 08:21:33 -0700

    0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-# Write an empty class Square that defines a square:
+"""Write an empty class Square that defines a square:"""
 
 
 class Square:
-    # This is an empty class
+    """ This is an empty class"""
     pass
