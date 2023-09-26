d04d814b810633f1a9c82363c88a03a849a456ed
AnyumbaKE <esanyumba@gmail.com>
2023-09-26 08:44:01 -0700

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Write a class Square that defines a square by:(based on 0-square.py"""
+"""Write a class Square that defines a square by:(based on 1-square.py"""
 
 
 class Square:
