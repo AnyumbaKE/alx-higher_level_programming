b4ded1e8ef89c4da5e94ca2b6cd8cc76f3d0dde0
Stan Anyumba <124333266+AnyumbaKE@users.noreply.github.com>
2023-09-26 20:06:41 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Write a class Square that defines a square by:(based on 3-square.py"""
+"""Write a class Square that defines a square by:(based on 3-square.py)"""
 
 
 class Square:
