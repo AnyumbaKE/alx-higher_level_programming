5ec9df9476679e77bce9ba1b7041087410721029
AnyumbaKE <esanyumba@gmail.com>
2023-10-02 10:29:04 -0700

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-A class Rectangle that defines a rectangle by: (based on 5-rectangle.py)
+A class Rectangle that defines a rectangle by: (based on 6-rectangle.py)
 """
 
 
@@ -17,19 +17,18 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """ Initialize rectangles """
+        """Initialize rectangles"""
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
-        type(self).number_of_instances += 1
 
     @property
     def width(self):
-        """ Returns width """
+        """Returns width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Sets width if int > 0 """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -38,12 +37,11 @@ class Rectangle:
 
     @property
     def height(self):
-        """ Gets returns height """
+        """Gets returns height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Sets height if int > 0 """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -52,7 +50,7 @@ class Rectangle:
 
     def area(self):
         """Returns the area"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def perimeter(self):
         """Returns the perimeter"""
@@ -66,14 +64,16 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
 
     def __repr__(self):
-        """Return a string representation of the rectangle"""
-        return "Rectangle({:d}, {:d})".format(self.width, self.height)
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
 
     def __del__(self):
         """Deletes instance of Rectangle"""
