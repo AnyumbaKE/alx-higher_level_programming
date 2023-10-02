46fb0bc8f9d16ba9146c13159ffb95cd585778da
AnyumbaKE <esanyumba@gmail.com>
2023-10-02 10:55:44 -0700

    10. N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.
+The N queens puzzle is the challenge of placing N
+non-attacking queens on an N×N chessboard.
 """
 import sys
 
