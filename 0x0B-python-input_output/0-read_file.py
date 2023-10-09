f792b396c7a3ac861c5a6f29ff129c2bcc95360d
AnyumbaKE <esanyumba@gmail.com>
2023-10-09 11:53:29 -0700

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that reads a text file (UTF8) and prints it to stdout."""
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
