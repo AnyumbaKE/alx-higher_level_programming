f35f6953e29100d8b32f189af3c445690c7b0dc6
AnyumbaKE <esanyumba@gmail.com>
2023-10-10 09:51:57 -0700

    8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure \
+            for JSON serialization of an object:.
+    """
+    return obj.__dict__
