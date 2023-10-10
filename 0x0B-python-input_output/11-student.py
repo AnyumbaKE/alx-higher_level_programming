58ac6fa52bd624af23d84cb503387bea308c9769
AnyumbaKE <esanyumba@gmail.com>
2023-10-10 11:17:12 -0700

    11-

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,54 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+
+Contains class Student
+that initializes public instance attributes first_name, last_name, and age,
+and has public method to_json that returns dictionary representation
+of requested attributes or all if none were requested
+"""
 
 
-class Student:
-    """Represent a student."""
+class Student():
+    """
+    Public Attributes:
+        first_name
+        last_name
+        age
 
+    Public Methods:
+        to_json: retrieves its dictionary representation
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a new student """
+        """
+        Initializes student with full name and age
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if (attrs is not None and
-                isinstance(attrs, list) and
-                all(isinstance(ele, str) for ele in attrs)):
-            return {k: getattrs(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+
+        Return:
+            Only return dict of attrs given to us
+            Return entire dict if no attrs given
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            dic = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    dic[att] = self.__dict__[att]
+            return dic
+
+    def reload_from_json(self, json):
+        """
+        Return:
+            Transfer all attributes of json to self
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
