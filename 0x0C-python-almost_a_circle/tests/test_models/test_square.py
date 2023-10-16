1cf2b2818c794f3de3e6332c8b7f027d5b455024
AnyumbaKE <esanyumba@gmail.com>
2023-10-16 10:41:37 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3>
-"""Defines unittests"""
+"""Defines unittests for models/square.py.
 
-
+Unittest classes:
+    TestSquare_instantiation - line 24
+    TestSquare_size - line 88
+    TestSquare_x - line 166
+    TestSquare_y - line 238
+    TestSquare_order_of_initialization - line 306
+    TestSquare_area - line 322
+    TestSquare_stdout - line 343
+    TestSquare_update_args - line 426
+    TestSquare_update_kwargs - line 538
+    TestSquare_to_dictionary - 640
+"""
 import io
 import sys
 import unittest
