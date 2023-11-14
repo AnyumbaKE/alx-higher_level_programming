51bb4401a5c5af93edb4e1ea98afa3f2dcc0af2d
AnyumbaKE <esanyumba@gmail.com>
2023-11-14 10:00:52 +0300

    1. Create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+/* A script that creates the database hbtn_0c_0 in MySQL server.
+   If the database hbtn_0c_0 already exists, the script should not fail */
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
