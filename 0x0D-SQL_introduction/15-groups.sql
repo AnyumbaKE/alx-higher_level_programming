1b611e4a2763208d7638800dc5e24674d45f29da
AnyumbaKE <esanyumba@gmail.com>
2023-11-14 11:22:12 +0300

    15. Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- A  script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in MySQL server.
- SELECT score,
+SELECT score,
     COUNT(*) as 'number'
 FROM second_table
 GROUP BY score
