215516a532b857ff94f1197651348b4e6a9a0df1
AnyumbaKE <esanyumba@gmail.com>
2023-11-14 12:07:14 +0300

    2. Delete a database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,2 @@
-/* A script that deletes the database hbtn_0c_0 in MySQL server.
-   If the database hbtn_0c_0 doesn't exists, the script should not fail */
+-- A script that deletes the database hbtn_0c_0 in MySQL server.If the database hbtn_0c_0 doesn't exists, the script should not fail
 DROP DATABASE IF EXISTS hbtn_0c_0;
