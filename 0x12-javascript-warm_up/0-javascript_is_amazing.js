7963df1899c0c7ed04dc8c512701f6996a74e21d
AnyumbaKE <esanyumba@gmail.com>
2023-12-11 18:27:50 +0300

    0. First constant, first print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,9 @@
+#!/usr/bin/Node
+/*
+A script that prints “JavaScript is amazing”:
+    Create a constant variable called myVar with the value “JavaScript is amazing”
+    Uses console.log(...) to print all output
+    We are not allowed to use var
+*/
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
