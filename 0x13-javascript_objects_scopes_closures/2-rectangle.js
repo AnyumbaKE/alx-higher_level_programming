053eda9dae879d8feacc2713f8b0d47821f45336
AnyumbaKE <esanyumba@gmail.com>
2023-12-12 00:42:23 -0800

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-	constructor (w, h) {
-		if((w>0) && (h>0)) {
-			this.width = w;
-			this.height = h;
-		}
-	}
-};
+// A class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
