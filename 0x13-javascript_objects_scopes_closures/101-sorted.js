910146c38a5f8d37809e3e4feb595c342a67fbe2
AnyumbaKE <esanyumba@gmail.com>
2023-12-12 01:56:02 -0800

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
+// A script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
 const dict = require('./101-data').dict;
-const newDiction = {};
 
-Object.keys(dict).map(function (key, index) {
-  if (newDiction[dict[key]] === undefined) {
-    newDiction[dict[key]] = [];
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
   }
-  newDiction[dict[key]].push(key);
-});
-
-console.log(newDiction);
+  newDict[valsUniq[j]] = list;
+}
+console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,6 +5,7 @@ module.exports = class Square extends square {
   constructor (size) {
     super(size, size);
   }
+
   charPrint (c) {
     const char = c || 'X';
     for (let i = 0; i < this.height; i++) {
