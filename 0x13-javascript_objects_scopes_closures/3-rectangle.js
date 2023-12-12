e4047a03434c29363fb67963f6f72d19447a895a
AnyumbaKE <esanyumba@gmail.com>
2023-12-12 11:57:42 +0300

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// A class Rectangle that defines a rectangle
+class Rectangle {
+    constructor (w, h) {
+      if ((w > 0) && (h > 0)) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+    print () {
+        for(let y = 0; y < this.height; y++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+  }
+  
+  module.exports = Rectangle;
