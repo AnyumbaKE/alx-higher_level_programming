e352f79ad545be856effaee62848bdd3d445a57f
AnyumbaKE <esanyumba@gmail.com>
2023-12-12 00:33:28 -0800

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-// A script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
 const dict = require('./101-data').dict;
 const newDiction = {};
 
