47d76dd28654582a8f6e29bce584235e29e726f2
AnyumbaKE <esanyumba@gmail.com>
2023-12-14 20:10:44 +0300

    0. Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A  script that lists all states from the database hbtn_0e_0_usa.
+        script should take 3 arguments:
+           mysql username
+           mysql password
+           database name 
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    row = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
