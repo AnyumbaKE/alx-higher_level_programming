3ac9b416c548f1cb907223268ba40a92a19c57f1
AnyumbaKE <esanyumba@gmail.com>
2023-12-18 00:30:38 +0300

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,8 +10,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT `cities.id`, `cities.name`, `states.name`
               FROM `cities`
