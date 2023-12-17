b59dd8483bdeed546bcbf9f60dbee05371c3e09c
AnyumbaKE <esanyumba@gmail.com>
2023-12-18 01:58:52 +0300

    14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that prints all City objects from the
+database hbtn_0e_14_usa
+        script should take 3 arguments:
+           mysql username
+           mysql password
+           database name
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
