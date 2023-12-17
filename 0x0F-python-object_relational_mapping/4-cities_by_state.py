f2baa0c3a5ab8728999ea37cf5a0fed6b8afdc05
AnyumbaKE <esanyumba@gmail.com>
2023-12-18 00:24:36 +0300

    4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities from the database hbtn_0e_4_usa
+        script should take 3 arguments:
+           mysql username
+           mysql password
+           database name
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("""SELECT `cities.id`, `cities.name`, `states.name`
+              FROM `cities`
+              JOIN `states` ON `cities.state_id` = `states.id`
+              ORDER BY `cities.id` ASC""")
+    [print(city) for city in c.fetchall()]
