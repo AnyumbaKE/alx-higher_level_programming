dd2d8b8b944eaf165929e1e7e6621670a64501e2
AnyumbaKE <esanyumba@gmail.com>
2023-12-17 04:03:29 -0800

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8", 
+    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` ORDER BY `id`")
