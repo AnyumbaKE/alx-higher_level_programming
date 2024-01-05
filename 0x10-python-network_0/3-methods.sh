8249301e6728f85f7091ca1931749f9f12438727
AnyumbaKE <esanyumba@gmail.com>
2024-01-05 14:32:06 -0800

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL and displays all
-# HTTP methods the server will accept.
+# A Bash script that takes in a URL and displays all HTTP methods the server will accept.
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
