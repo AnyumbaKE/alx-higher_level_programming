e30ea499d73652eea9523fe1acf0be32ffa34a1f
AnyumbaKE <esanyumba@gmail.com>
2024-01-05 14:34:50 -0800

    4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL as an argument, sends
-# a GET request to the URL, and displays the body of the response
+# A Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -s "$1" -H "X-School-User-Id: 98"
