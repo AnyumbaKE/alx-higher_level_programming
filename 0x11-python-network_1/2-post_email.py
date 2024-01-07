982b53e11c36756875212f0b3c5dec4daad9e1aa
AnyumbaKE <esanyumba@gmail.com>
2024-01-07 12:37:11 +0300

    task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL and an email, sends a POST request
+    to the passed URL with the email as a parameter, and displays the
+        body of the response (decoded in utf-8)
+
+- The email is sent in the email variable
+- Am using the packages urllib and sys
+- Am not allowed to import packages other than urllib and sys
+- Am checking arguments passed to the script (number or type)
+"""
+
+
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
