1d929aee19285a3708172afa2d6c40418fe77933
AnyumbaKE <esanyumba@gmail.com>
2024-01-07 05:22:23 -0800

    3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,7 +3,7 @@
 A Python script that takes in a URL, sends a request to the URL
     and displays the body of the response (decoded in utf-8).
 
-- You have to manage urllib.error.HTTPError exceptions and 
+- You have to manage urllib.error.HTTPError exceptions and
     print: Error code: followed by the HTTP status code
 - Am using the packages urllib and sys
 - Am not allowed to import packages other than urllib and sys
