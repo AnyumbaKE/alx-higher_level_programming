df7f5d50b2d9351b9a3e54d7671ae48073fd5166
AnyumbaKE <esanyumba@gmail.com>
2024-01-07 12:31:06 +0300

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,9 @@
 """
 Write a Python script that fetches https://alx-intranet.hbtn.io/status
 
-- You must use the package urllib
-- You are not allowed to import any packages other than urllib
-- The body of the response must be displayed like the following example (tabulation before -)
-You must use a with statement
+- Am using the package urllib
+- Am not allowed to import any packages other than urllib
+- The body of the response is displayed like the following example (tabulation before -)
 """
 
 
