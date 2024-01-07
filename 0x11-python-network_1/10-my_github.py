86cb687594e616afdcdf5ea878f50e07b08b7235
AnyumbaKE <esanyumba@gmail.com>
2024-01-07 17:12:11 +0300

    task9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A Python script that takes your GitHub credentials (username and password)
+    and uses the GitHub API to display your id
+
+- Am using Basic Authentication with a personal access token as password for access
+- The first argument is username
+- The second argument is password
+- Am using the packages requests and sys
+- Am not allowed to import packages other than requests and sys
+- Am not error checking arguments passed to the script (number or type)
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    rqst = requests.get("https://api.github.com/user", auth=auth)
+    print(rqst.json().get("id"))
