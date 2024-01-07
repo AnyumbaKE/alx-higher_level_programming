b550ffb41650df8cb7bdbaa56d6052a16cd95fa0
AnyumbaKE <esanyumba@gmail.com>
2024-01-07 12:54:17 +0300

    task4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches https://alx-intranet.hbtn.io/status
+
+- Am using the package requests
+- Am not allowed to import packages other than requests
+- The body of the response must be display like the following example (tabulation before -)
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
